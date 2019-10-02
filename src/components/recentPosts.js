c1306e7a263b724ca4f25d507e732160e9efb1bf
Chandler King <ckmotox356@gmail.com>
2019-10-01 19:38:04 -0600

    used axios to fetch the recent posts in the recentPosts component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 
 export default class RecentPosts extends Component {
   constructor() {
   super();
+
+  this.getRecentPosts = this.getRecentPosts.bind(this);
 }
+
+getRecentPosts() {
+  axios.get("https://api.dailysmarty.com/posts").then(response => {
+    console.log("get recent posts response", response);
+  }
+  ).catch(error => {
+    console.log("Get recent posts error", error);
+  });
+}
+
+componentDidMount() {
+  this.getRecentPosts();
+}
+
+
   render() {
     return(
         <div className="recent-posts-wrapper">
