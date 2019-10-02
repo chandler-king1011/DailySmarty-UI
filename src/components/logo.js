74a91ab9e785a7921137671a36548076aeee335a
Chandler King <ckmotox356@gmail.com>
2019-10-01 18:01:20 -0600

    Initiated a logo component and added some corresponding styles

---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import logo from '../../static/assets/images/ds_circle_logo.png';
+
+export default function() {
+  return (
+    <div className="logo-wrapper-main">
+        <img alt="logo" src={logo}/>
+    </div>
+)
+}
