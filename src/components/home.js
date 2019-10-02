a51740b29abdf553a2636eaeb1e48d35823993b2
Chandler King <ckmotox356@gmail.com>
2019-10-01 19:04:14 -0600

    Used the withRouter HOC in the searchbar to push the user to the results page

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ import Logo from "./logo.js";
 import SearchBar from "./searchBar.js";
 import RecentPosts from "./recentPosts.js";
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div className='app'>
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+import { withRouter } from 'react-router-dom';
+
 class SearchBar extends Component {
   constructor() {
   super();
@@ -10,8 +12,10 @@ class SearchBar extends Component {
 
 
   handleFormSubmit = function({query}) {
-      console.log("Trying to handle submit for query", query)
+      console.log("Trying to handle submit for query", query);
+      this.props.history.push('/results');
   }
+
   renderInput(field) {
     return <input type="text" placeholder="Search DailySmarty" {...field.input} />
   }
@@ -33,6 +37,8 @@ class SearchBar extends Component {
 SearchBar = reduxForm({
     form: 'searchBar'
 })(SearchBar);
+
+SearchBar=withRouter(SearchBar);
 
 export default SearchBar;
 
